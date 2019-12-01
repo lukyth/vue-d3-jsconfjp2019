87fc1244ab67e8be1eab2cfe4b960d7e2d430b40
Kanitkorn S <k.sujautra@gmail.com>
2019-12-01 10:59:36 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,94 @@
+<template>
+  <div id="app">
+    <!-- HEADER -->
+    <h1>The Seasonality of Box Office Hits</h1>
+    <p class='description'>
+In the last decade, most block buster hits have happened around or during the summer and winter holidays.
+<strong>Brush</strong> the histograms to filter the movies by metascore and/or box office figures, and <strong>hover</strong> the movies to see more details.
+    </p>
+    <!-- AREA CHART -->
+    <div class='area-container'>
+      <div class='area-title'>
+        <strong>$ over/under median box office</strong>
+      </div>
+      <AreaChart v-bind='{}' />
+    </div>
+    <!-- HISTOGRAM -->
+    <div class='histograms' v-for='({id, label, format}) in histograms'>
+      <Histogram v-bind='{movies, filtered, id, format, updateFilters}' />
+      <div>
+        <strong>{{ label }}</strong>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+import * as d3 from 'd3'
+import AreaChart from './components/AreaChart'
+import Histogram from './components/Histogram'
+
+const startYear = 2008
+export default {
+  name: 'app',
+  components: {
+    Histogram, AreaChart,
+  },
+  data() {
+    // register any data we want to track changes of
+    return {
+      histograms: [
+        {id: 'score', label: 'metascores'},
+        {id: 'boxOffice', label: 'box office figures', format: d => `$${parseInt(d/ 1000000)}M`},
+      ],
+      movies: [],
+      filters: {},
+      filtered: [],
+    }
+  },
+  mounted() {
+    fetch(`./movies.json`)
+      .then(resp => resp.json())
+      .then(movies => {
+        this.movies = _.chain(movies)
+          .map((d, id) => Object.assign(d, {id, date: new Date(d.date)}))
+          .filter(d => d.boxOffice && d.year >= startYear)
+          .value();
+        this.filtered = this.movies
+      });
+  },
+  methods: {
+    updateFilters: function(filter) {
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  width: 1000px;
+  margin: auto;
+  text-align: center;
+}
+
+.description {
+  width: 600px;
+  line-height: 2;
+  margin: 20px auto;
+}
+
+.area-container {
+  position: relative;
+}
+
+.area-title {
+  position: absolute;
+  left: 65px;
+}
+
+.histograms {
+  display: inline-block;
+}
+</style>
---
+++ src/components/AreaChart.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class='areachart'>
+    <svg :width='width' :height='height'>
+      <!-- MOVIE ARCS -->
+      <!-- AXES -->
+      <g ref='xAxis' :transform='`translate(0, ${y0})`' />
+      <g ref='yAxis' :transform='`translate(${margin.left}, 0)`' />
+    </svg>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+import * as d3 from 'd3'
+import {TweenLite} from 'gsap'
+
+const width = 1000
+const height = 300
+const margin = {top: 0, right: 0, bottom: 20, left: 60}
+
+export default {
+  name: 'areachart',
+  props: [],
+  data() {
+    return {
+      width, height, margin,
+    }
+  },
+  watch: {
+  },
+  methods: {
+    renderAxes: function() {
+      if (!this.xScale || !this.yScale) return
+
+      const xAxis = d3.axisBottom().scale(this.xScale).tickFormat(this.format)
+      const yAxis = d3.axisLeft().scale(this.yScale)
+        .tickSizeOuter(0)
+        .tickFormat(d => `${d3.format('$')(parseInt(d / 1000000))}M`)
+
+      // call axes on group elements
+      d3.select(this.$refs.xAxis).call(xAxis)
+      d3.select(this.$refs.yAxis).call(yAxis)
+        .selectAll('path').remove()
+    },
+  }
+}
+</script>
+
+<style>
+.areachart {
+  position: relative;
+}
+
+.arc {
+  cursor: pointer;
+}
+</style>
