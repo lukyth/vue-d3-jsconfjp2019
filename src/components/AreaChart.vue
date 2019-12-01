91e6d7e51a78f66943bf2a452d4ed905625239d2
Kanitkorn S <k.sujautra@gmail.com>
2019-12-01 12:46:27 +0900

    Make areachart work

--- src/components/AreaChart.vue
+++ src/components/AreaChart.vue
@@ -2,6 +2,7 @@
   <div class='areachart'>
     <svg :width='width' :height='height'>
       <!-- MOVIE ARCS -->
+      <path v-for='d in arcs' :d='d.path' :fill='d.fill' stroke='#fff' />
       <!-- AXES -->
       <g ref='xAxis' :transform='`translate(0, ${y0})`' />
       <g ref='yAxis' :transform='`translate(${margin.left}, 0)`' />
@@ -12,7 +13,6 @@
 <script>
 import _ from 'lodash'
 import * as d3 from 'd3'
-import {TweenLite} from 'gsap'
 
 const width = 1000
 const height = 300
@@ -20,15 +20,62 @@ const margin = {top: 0, right: 0, bottom: 20, left: 60}
 
 export default {
   name: 'areachart',
-  props: [],
+  props: ['movies'],
   data() {
     return {
       width, height, margin,
+      y0: 0,
+      arcs: [],
     }
   },
   watch: {
+    movies() {
+      this.calculateScale()
+      this.calculateDate()
+      this.renderAxes()
+    }
   },
   methods: {
+    calculateScale() {
+      if (!this.movies.length) return
+
+      const [minDate, maxDate] = d3.extent(this.movies, d => d.date)
+      this.xScale = d3.scaleTime()
+        .domain([d3.timeMonth.offset(minDate, -2), d3.timeMonth.offset(maxDate, 2)])
+        .range([margin.left, width - margin.right])
+
+      this.medianBox = d3.median(this.movies, d => d.boxOffice)
+
+      const boxOfficeExtent = d3.extent(this.movies, d => d.boxOffice - this.medianBox)
+      this.yScale = d3.scaleLinear()
+        .domain(boxOfficeExtent)
+        .range([height - margin.bottom, margin.top])
+      this.y0 = this.yScale(0)
+
+      const scoreExtent = d3.extent(this.movies, d => d.score)
+      this.colorScale = d3.scaleSequential()
+        .domain(scoreExtent)
+        .interpolator(d3.interpolateViridis)
+      
+      this.area = d3.area()
+        .y0(this.y0)
+        .curve(d3.curveCatmullRom)
+    },
+    calculateDate() {
+      if (!this.movies.length) return
+
+      this.arcs = this.movies.map(d => {
+        return {
+          id: d.id,
+          path: this.area([
+            [this.xScale(d3.timeMonth.offset(d.date, -2)), this.y0],
+            [this.xScale(d.date), this.yScale(d.boxOffice - this.medianBox)],
+            [this.xScale(d3.timeMonth.offset(d.date, 2)), this.y0],
+          ]),
+          fill: this.colorScale(d.score),
+        }
+      })
+    },
     renderAxes: function() {
       if (!this.xScale || !this.yScale) return
 
