0f92a6a8202a29bb2c81d2fdaae879af69f8eb9b
Kanitkorn S <k.sujautra@gmail.com>
2019-12-13 02:46:24 +0900

    Add eslint and prettier

--- src/App.vue
+++ src/App.vue
@@ -2,46 +2,55 @@
   <div id="app">
     <!-- HEADER -->
     <h1>The Seasonality of Box Office Hits</h1>
-    <p class='description'>
-In the last decade, most block buster hits have happened around or during the summer and winter holidays.
-<strong>Brush</strong> the histograms to filter the movies by metascore and/or box office figures, and <strong>hover</strong> the movies to see more details.
+    <p class="description">
+      In the last decade, most block buster hits have happened around or during the summer and
+      winter holidays.
+      <strong>Brush</strong> the histograms to filter the movies by metascore and/or box office
+      figures, and <strong>hover</strong> the movies to see more details.
     </p>
     <!-- AREA CHART -->
-    <div class='area-container'>
-      <div class='area-title'>
+    <div class="area-container">
+      <div class="area-title">
         <strong>$ over/under median box office</strong>
       </div>
-      <AreaChart v-bind='{movies, filtered}' />
+      <AreaChart v-bind="{ movies, filtered }" />
     </div>
     <!-- HISTOGRAM -->
-    <div class='histograms' v-for='({id, label, format}) in histograms'>
-      <Histogram v-bind='{movies, filtered, id, format, updateFilters}' />
+    <div
+      v-for="{ id, label, format } in histograms"
+      :key="id"
+      class="histograms"
+    >
+      <Histogram v-bind="{ movies, filtered, id, format, updateFilters }" />
       <div>
         <strong>{{ label }}</strong>
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
 import _ from 'lodash'
-import * as d3 from 'd3'
 import AreaChart from './components/AreaChart'
 import Histogram from './components/Histogram'
 
 const startYear = 2008
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    Histogram, AreaChart,
+    Histogram,
+    AreaChart,
   },
   data() {
     // register any data we want to track changes of
     return {
       histograms: [
-        {id: 'score', label: 'metascores'},
-        {id: 'boxOffice', label: 'box office figures', format: d => `$${parseInt(d/ 1000000)}M`},
+        { id: 'score', label: 'metascores' },
+        {
+          id: 'boxOffice',
+          label: 'box office figures',
+          format: (d) => `$${parseInt(d / 1000000)}M`,
+        },
       ],
       movies: [],
       filters: {},
@@ -50,29 +59,27 @@ export default {
   },
   mounted() {
     fetch(`./movies.json`)
-      .then(resp => resp.json())
-      .then(movies => {
+      .then((resp) => resp.json())
+      .then((movies) => {
         this.movies = _.chain(movies)
-          .map((d, id) => Object.assign(d, {id, date: new Date(d.date)}))
-          .filter(d => d.boxOffice && d.year >= startYear)
-          .value();
+          .map((d, id) => Object.assign(d, { id, date: new Date(d.date) }))
+          .filter((d) => d.boxOffice && d.year >= startYear)
+          .value()
         this.filtered = this.movies
-      });
+      })
   },
   methods: {
     updateFilters: function(filter) {
       this.filters = Object.assign(this.filters, filter)
-      console.log(this.filters)
-      this.filtered = this.movies.filter(d => 
+      this.filtered = this.movies.filter((d) =>
         _.every(this.filters, (bounds, type) => {
           if (!bounds) return true
 
           return bounds[0] < d[type] && d[type] < bounds[1]
         })
       )
-      console.log(this.filtered)
-    }
-  }
+    },
+  },
 }
 </script>
 
--- src/components/AreaChart.vue
+++ src/components/AreaChart.vue
@@ -1,29 +1,45 @@
 <template>
-  <div class='areachart'>
-    <svg :width='width' :height='height'>
+  <div class="areachart">
+    <svg
+      :width="width"
+      :height="height"
+    >
       <!-- MOVIE ARCS -->
-      <path v-for='d in arcs' :d='d.path' :fill='d.fill' stroke='#fff' />
+      <path
+        v-for="d in arcs"
+        :key="d.path"
+        :d="d.path"
+        :fill="d.fill"
+        stroke="#fff"
+      />
       <!-- AXES -->
-      <g ref='xAxis' :transform='`translate(0, ${y0})`' />
-      <g ref='yAxis' :transform='`translate(${margin.left}, 0)`' />
+      <g
+        ref="xAxis"
+        :transform="`translate(0, ${y0})`"
+      />
+      <g
+        ref="yAxis"
+        :transform="`translate(${margin.left}, 0)`"
+      />
     </svg>
   </div>
 </template>
 
 <script>
-import _ from 'lodash'
 import * as d3 from 'd3'
 
 const width = 1000
 const height = 300
-const margin = {top: 0, right: 0, bottom: 20, left: 60}
+const margin = { top: 0, right: 0, bottom: 20, left: 60 }
 
 export default {
-  name: 'areachart',
+  name: 'Areachart',
   props: ['movies', 'filtered'],
   data() {
     return {
-      width, height, margin,
+      width,
+      height,
+      margin,
       y0: 0,
       arcs: [],
     }
@@ -36,38 +52,42 @@ export default {
     },
     filtered() {
       this.calculateDate()
-    }
+    },
   },
   methods: {
     calculateScale() {
       if (!this.movies.length) return
 
-      const [minDate, maxDate] = d3.extent(this.movies, d => d.date)
-      this.xScale = d3.scaleTime()
+      const [minDate, maxDate] = d3.extent(this.movies, (d) => d.date)
+      this.xScale = d3
+        .scaleTime()
         .domain([d3.timeMonth.offset(minDate, -2), d3.timeMonth.offset(maxDate, 2)])
         .range([margin.left, width - margin.right])
 
-      this.medianBox = d3.median(this.movies, d => d.boxOffice)
+      this.medianBox = d3.median(this.movies, (d) => d.boxOffice)
 
-      const boxOfficeExtent = d3.extent(this.movies, d => d.boxOffice - this.medianBox)
-      this.yScale = d3.scaleLinear()
+      const boxOfficeExtent = d3.extent(this.movies, (d) => d.boxOffice - this.medianBox)
+      this.yScale = d3
+        .scaleLinear()
         .domain(boxOfficeExtent)
         .range([height - margin.bottom, margin.top])
       this.y0 = this.yScale(0)
 
-      const scoreExtent = d3.extent(this.movies, d => d.score)
-      this.colorScale = d3.scaleSequential()
+      const scoreExtent = d3.extent(this.movies, (d) => d.score)
+      this.colorScale = d3
+        .scaleSequential()
         .domain(scoreExtent)
         .interpolator(d3.interpolateViridis)
-      
-      this.area = d3.area()
+
+      this.area = d3
+        .area()
         .y0(this.y0)
         .curve(d3.curveCatmullRom)
     },
     calculateDate() {
       if (!this.filtered.length) return
 
-      this.arcs = this.filtered.map(d => {
+      this.arcs = this.filtered.map((d) => {
         return {
           id: d.id,
           path: this.area([
@@ -82,17 +102,24 @@ export default {
     renderAxes: function() {
       if (!this.xScale || !this.yScale) return
 
-      const xAxis = d3.axisBottom().scale(this.xScale).tickFormat(this.format)
-      const yAxis = d3.axisLeft().scale(this.yScale)
+      const xAxis = d3
+        .axisBottom()
+        .scale(this.xScale)
+        .tickFormat(this.format)
+      const yAxis = d3
+        .axisLeft()
+        .scale(this.yScale)
         .tickSizeOuter(0)
-        .tickFormat(d => `${d3.format('$')(parseInt(d / 1000000))}M`)
+        .tickFormat((d) => `${d3.format('$')(parseInt(d / 1000000))}M`)
 
       // call axes on group elements
       d3.select(this.$refs.xAxis).call(xAxis)
-      d3.select(this.$refs.yAxis).call(yAxis)
-        .selectAll('path').remove()
+      d3.select(this.$refs.yAxis)
+        .call(yAxis)
+        .selectAll('path')
+        .remove()
     },
-  }
+  },
 }
 </script>
 
